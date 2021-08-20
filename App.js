51b358614735a9314a7938acb70f44175d96c236
stephanie-warmenhoven <steph_war@yahoo.ca>
2021-08-20 11:06:34 -0700

    fetch pokemon with hooks and fetch request to pokemon api

--- App.js
+++ App.js
@@ -1,9 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Header from './components/Header'
+import Header from './components/Header';
+
+const POKEMON_API_ENDPOINT = 'https://pokeapi.co/api/v2/';
+const FIRST_100_POKEMON_QUERY = 'pokemon?limit=100';
 
 export default function App() {
+  const [pokemonList, setPokemonList] = useState([]);
+
+  useEffect(()=> {
+    fetch(`${POKEMON_API_ENDPOINT}${FIRST_100_POKEMON_QUERY}`)
+    .then(res => res.json())
+    .then(payload => {
+      console.log(payload);
+    })
+  }, [])
   return (
     <View style={styles.container}>
       <Header/>
