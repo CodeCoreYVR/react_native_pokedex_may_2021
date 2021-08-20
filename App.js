096a85d6ecc44d9ae8a33efc81b6297fad6a5c7d
stephanie-warmenhoven <steph_war@yahoo.ca>
2021-08-20 10:35:19 -0700

    basic react-native app with some styling

--- App.js
+++ App.js
@@ -5,7 +5,12 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! hello world</Text>
+      <View style={styles.header}>
+        <Text style={styles.h1}>Pokédex</Text>
+      </View>
+      <View style={styles.body}>
+        <Text>Hello world</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,10 +19,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'stretch',
     justifyContent: 'center',
   },
+  header: {
+    flex: 1,
+    backgroundColor: 'red',
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 25,
+    borderBottomWidth: 15
+  },
+  body: {
+    flex: 5
+  },
+  h1: {
+    fontSize: 40
+  }
 });
 
 
